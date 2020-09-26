db71fee35a678ef029d636b6ddfa5cc0e5bc9f68
John-Ryan-Johnson <john.r.johnsondev@gmail.com>
2020-09-26 10:16:21 -0500

    Add files via upload

---
+++ Northwind_answer1.sql
@@ -0,0 +1,9 @@
+--1. What is the undiscounted subtotal for each Order (identified by OrderID).
+
+select orders.orderid as [Order ID], orders.customerid as [Customer ID], [Order Details].unitprice * [Order Details].quantity as [Undiscounted Subtotal]
+	from orders
+		join [Order Details]
+		on orders.orderid = [Order Details].orderid
+
+
+
---
+++ Northwind_answer2.sql
@@ -0,0 +1,8 @@
+--2. What products are currently for sale (not discontinued)?
+
+select *
+from products
+	where discontinued = 0
+
+
+
